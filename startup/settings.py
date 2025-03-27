6c8c8c1f24a3073b71eab490b28093cd2f913804
BoomeshKS <boomeshalone@gmail.com>
2025-03-27 06:39:13 +0530

    Fixed template not found issue & static settings

--- startup/settings.py
+++ startup/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'startup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Altruisty', 'templates')], 
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
